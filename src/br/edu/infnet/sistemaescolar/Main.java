fb99295c485c08526f55212239c09c6271ec6693
Bianca Gotaski <biancagotaski@outlook.com.br>
2016-11-01 11:05:11 -0200

    Primeira Opção parcialmente concluida

--- src/br/edu/infnet/sistemaescolar/Main.java
+++ src/br/edu/infnet/sistemaescolar/Main.java
@@ -8,7 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         PrintStream printStream = new PrintStream(System.out);
-
+        
+        String[] alunos = new String[100];
+        Double[] av1 = new Double[100];
+        Double[] av2 = new Double[100];
+        
         printStream.println("[1] Registrar as notas de um aluno");
         printStream.println("[2] Consultar boletim de um aluno");
         printStream.println("[3] Consultar notas da turma");
@@ -16,7 +20,7 @@ public class Main {
 
 
         int itemMenuEscolhido = Integer.parseInt(scanner.nextLine());
-
+        
         switch (itemMenuEscolhido){
             case 1:
                 printStream.print("Nome do aluno: ");
@@ -25,6 +29,21 @@ public class Main {
                 Double nota1 = Double.parseDouble(scanner.nextLine());
                 printStream.print("Nota da AV2: ");
                 Double nota2 = Double.parseDouble(scanner.nextLine());
+                
+                int indiceDisponivel = 0;
+                
+                for (int indice = 0; indice < alunos.length; indice++){
+                    if(alunos[indice] == null) {
+                        indiceDisponivel = indice;
+                        break;
+                    }
+                }
+                
+                alunos[indiceDisponivel] = nome;
+                av1[indiceDisponivel] = nota1;
+                av2[indiceDisponivel] = nota2;
+                printStream.println("Código do aluno: " + indiceDisponivel);
+                                
                 break;
 
         }
